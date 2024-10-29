21d8cebffc93a523f1e77c25ccb590b041b5b3e5
tahyo27 <tahyo27@gmail.com>
2024-10-29 21:28:47 +0900

    Head First init

---
+++ HeadFirstInit/HeadFirstInit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HeadFirstInit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Head First App");
+        }
+    }
+}
